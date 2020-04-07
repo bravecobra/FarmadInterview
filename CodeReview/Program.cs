982dcf04db94362f8e1961e1a681fe22f6a6e9b0
Marco Van Hoorick <m.vanhoorick@farmad.be>
2020-04-07 08:43:45 +0200

    Add project files.

---
+++ CodeReview/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+// ReSharper disable All
+
+namespace CodeReview
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var b = ReadFirstByte(args[0]).Result;
+            return b;
+        }
+
+        /// <summary> Reads the first byte. </summary>
+        static async Task<int> ReadFirstByte(string? fn)
+        {
+            var fileStream = File.Open(fn, FileMode.Open);
+
+            try
+            {
+                var result = new byte[1];
+                await fileStream.ReadAsync(result, offset: 0, count: 1);
+                return result[0];
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                throw ex;
+            }
+        }
+    }
+
+    public static class Logger
+    {
+        public static void LogError(string errorMessage)
+        {
+            Console.WriteLine($"[ERR] {errorMessage}");
+        }
+    }
+}
---
+++ LinqPuzzle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+// ReSharper disable All
+
+namespace LinqPuzzle
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var items = GetItems().Skip(1).Take(2);
+
+            Console.WriteLine("Retrieved all items");
+            Console.WriteLine($"Count: {await items.CountAsync()}");
+            await foreach (var item in items)
+            {
+                Console.WriteLine($"Item: {item}");
+            }
+        }
+
+        public static async IAsyncEnumerable<string> GetItems()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                await Task.Delay(100);
+                Console.WriteLine($"Yielding {i + 1}");
+                yield return $"{i + 1}";
+            }
+        }
+    }
+}
